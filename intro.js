984737ddb7ebb0a3d4db142c126a2a1f57d70e47
Florentina Vasilovschi <fvasilovschi@gmail.com>
2019-03-18 20:34:28 +0200

    extract function printSkill

--- intro.js
+++ intro.js
@@ -1,17 +1,15 @@
-function getWelcomeMsg(){
-    return "Welcome to my site!";
-    
+function getWelcomeMsg() {
+  return "Welcome to my site!";
 }
 
 function getColor() {
-    var date = new Date();
-    console.log(date.getMinutes());
-    if (date.getMinutes() > 9) {
-        return "green"
-    }
-    return "red";
+  var date = new Date();
+  console.log(date.getMinutes());
+  if (date.getMinutes() > 9) {
+    return "green";
+  }
+  return "red";
 }
-
 
 var welcome = getWelcomeMsg();
 console.info(welcome);
@@ -22,43 +20,47 @@ console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
 
-
 function hidePage(page) {
-    document.getElementById(page).style.display = 'none';
+  document.getElementById(page).style.display = "none";
 }
 function showPage(page) {
-    document.getElementById(page).style.display = 'block';
+  document.getElementById(page).style.display = "block";
 }
 
 function initMenu() {
-    var links = document.querySelectorAll("#top-menu-bar a");
-        console.info(links);
-    for (var i = 0; i < links.length; i++) {      
-        links[i].onclick = clickOnMenuItem;
-    }
+  var links = document.querySelectorAll("#top-menu-bar a");
+  console.info(links);
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = clickOnMenuItem;
+  }
 }
-
 
 function clickOnMenuItem() {
-    hideAllPages();
-    var pageId = this.getAttribute('data-page');
-    showPage(pageId);
+  hideAllPages();
+  var pageId = this.getAttribute("data-page");
+  showPage(pageId);
 }
 
-function hideAllPages(){
-    var pages = document.querySelectorAll('.page');
-    for (var i = 0; i < pages.length; i++){
-        pages[i].style.display = 'none';
-    }
+function hideAllPages() {
+  var pages = document.querySelectorAll(".page");
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
 }
 
-initMenu ();
+initMenu();
 
-function showSkills(){
+function showSkills() {
     var skills = ['html', 'css', 'js'];
     console.warn('showSkills', skills);
-    for(var i = 0; i < skills.length; i++)
-    console.info(i + 1 + ". " + skills[i]);
-}
+    for(var i = 0; i < skills.length; i++) {
+        printSkill(skills[i], i);
+    }
 
+    function printSkill(skill, index) {
+        console.info("#" + (index + 1) + " " + skill);
+    }
+
+    skills.forEach(printSkill);
+}
 showSkills();
